d695bd355a2b1c1ea210fbb11964d00c77a7e365
OKE15270 <nicholas.farley@oneok.com>
2017-10-18 11:00:48 -0500

    Adding log4net configuration from Common.Logging.Log4Net1215. Slightly modified to keep default behavior of Common.Logging.Log4Net.Universal.

--- Common.Logging.Log4Net.Universal/Log4NetFactoryAdapter.cs
+++ Common.Logging.Log4Net.Universal/Log4NetFactoryAdapter.cs
@@ -13,22 +13,59 @@
 // limitations under the License.
 
 using System;
-using Common.Logging;
+using Common.Logging.Configuration;
+using System.IO;
 
-namespace Common.Logging.Log4Net.Universal
-{
+namespace Common.Logging.Log4Net.Universal {
     /// <summary>
     /// Serilog factory adapter.
     /// </summary>
-    public class Log4NetFactoryAdapter : ILoggerFactoryAdapter
-    {
-        public ILog GetLogger(Type type)
-        {
+    public class Log4NetFactoryAdapter : ILoggerFactoryAdapter {
+
+        public Log4NetFactoryAdapter() : this(null) {}
+
+        public Log4NetFactoryAdapter(NameValueCollection properties) {
+            string configType = ArgUtils.GetValue(properties, "configType", string.Empty).ToUpper();
+            string configFile = ArgUtils.GetValue(properties, "configFile", string.Empty);
+
+            // app-relative path?
+            if (configFile.StartsWith("~/") || configFile.StartsWith("~\\")) {
+                configFile = string.Format("{0}/{1}", AppDomain.CurrentDomain.BaseDirectory.TrimEnd('/', '\\'), configFile.Substring(2));
+            }
+
+            if (configType == "FILE" || configType == "FILE-WATCH") {
+                if (configFile == string.Empty) {
+                    throw new ConfigurationException("Configuration property 'configFile' must be set for log4Net configuration of type 'FILE' or 'FILE-WATCH'.");
+                }
+
+                if (!File.Exists(configFile)) {
+                    throw new ConfigurationException("log4net configuration file '" + configFile + "' does not exists");
+                }
+            }
+
+            switch (configType) {
+                case "INLINE":
+                    log4net.Config.XmlConfigurator.Configure();
+                    break;
+                case "FILE":
+                    log4net.Config.XmlConfigurator.Configure(new FileInfo(configFile));
+                    break;
+                case "FILE-WATCH":
+                    log4net.Config.XmlConfigurator.ConfigureAndWatch(new FileInfo(configFile));
+                    break;
+                case "EXTERNAL":
+                    // Log4net will be configured outside of Common.Logging
+                default:
+                    //log4net.Config.XmlConfigurator.Configure();
+                    break;
+            }
+        }
+
+        public ILog GetLogger(Type type) {
             return new Log4NetLogger(log4net.LogManager.GetLogger(type));
         }
 
-        public ILog GetLogger(string name)
-        {
+        public ILog GetLogger(string name) {
             return new Log4NetLogger(log4net.LogManager.GetLogger(name));
         }
     }
